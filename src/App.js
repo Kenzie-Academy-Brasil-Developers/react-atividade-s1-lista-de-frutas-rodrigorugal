48b6afdf426c6bf294d19166bdf67e4e46db1422
rodrigo luiz <rodrigoluizandradedeoliveira@gmail.com>
2021-09-01 23:59:14 -0300

    em andamento

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Fruits from "./components/carFruits/Fruits";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Fruits />
+      </header>
+    </div>
+  );
+}
+
+export default App;
